2e5c754028d74b9412052b6395022ba17dda3aaa
User <souhaib.aljaziri@gmail.com>
2017-10-22 01:47:54 +0100

    profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -2,7 +2,28 @@ import React, { Component } from "react";
 
 export default class Profile extends Component {
   render() {
-    return <div>{this.props.params.id}</div>;
+  return (
+  
+    <div class="container">
+        <div class="fb-profile">
+            <img align="left" class="fb-image-lg" src="https://scontent.ftun2-1.fna.fbcdn.net/v/t1.0-9/16142561_1396825030368915_2561193169003175237_n.jpg?oh=4b8d12a9d32bc8bb5f5148c73a82cf4d&oe=5A645FBB" alt="Profile image example"/>
+            <img align="left" class="fb-image-profile thumbnail" src="https://scontent.ftun2-1.fna.fbcdn.net/v/t1.0-9/21231643_1653978394653576_3776291800751053715_n.jpg?oh=93d1b065bfc948c4a4c17e0557e99f2b&oe=5A6E29F6" alt="Profile image example"/>
+            <div class="fb-profile-text">
+                <h1>souhaib eljaziri</h1>
+                <p><div>{this.props.params.id}</div></p>
+            </div>
+        </div>
+        <div class="maincontent">
+        
+<div class="icon-bar">
+  <a class="active" href="#"><i>Journal</i></a> 
+  <a href="#"><i>Apropos</i></a> 
+  <a href="#"><i>Amis</i></a> 
+  <a href="#"><i>photo</i></a>
+    </div>
+</div>
+</div>
+    );
   }
   
 }
