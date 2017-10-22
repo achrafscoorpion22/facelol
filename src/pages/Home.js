b24786fcad7f9b494eb4559d934af8b5d69ff1d5
omair2010 <omar23655@hotmail.fr>
2017-10-22 01:16:59 +0100

    ahla cv chnewa jawlpù el mohem chouf hedhi kifeh

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,9 @@ import * as firebase from "firebase";
 import PostInput from "../component/Home/PostInput";
 
 export default class Home extends Component {
+  state={
+      chedli:""
+  }
   componentWillMount = () => {
     firebase
       .firestore()
@@ -10,16 +13,21 @@ export default class Home extends Component {
       .get()
       .then(querry => {
         querry.forEach(doc => {
-          console.log(doc.data().user);
+          if(doc.data().user==="8QT69sh719OfkUdhfPERXad0SZg1"){this.setState({chedli:doc.data().statu})
+                                                  {this.state.chedli}        }
         });
       });
+
+
+
+
   };
 
   render() {
     return (
       <div>
         <PostInput />
-        achraf
+        
       </div>
     );
   }
