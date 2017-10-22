8793ecea58784590a743433660d16bccd011b39e
User <souhaib.aljaziri@gmail.com>
2017-10-22 23:12:11 +0100

    profile disscusion

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
 
 export default class Profile extends Component {
+    
   render() {
+
   return (
   
     <div class="container">
+        <div className="cadre">
         <div class="fb-profile">
             <img align="left" class="fb-image-lg" src="https://scontent.ftun2-1.fna.fbcdn.net/v/t1.0-9/16142561_1396825030368915_2561193169003175237_n.jpg?oh=4b8d12a9d32bc8bb5f5148c73a82cf4d&oe=5A645FBB" alt="Profile image example"/>
             <img align="left" class="fb-image-profile thumbnail" src="https://scontent.ftun2-1.fna.fbcdn.net/v/t1.0-9/21231643_1653978394653576_3776291800751053715_n.jpg?oh=93d1b065bfc948c4a4c17e0557e99f2b&oe=5A6E29F6" alt="Profile image example"/>
@@ -21,6 +24,7 @@ export default class Profile extends Component {
   <a href="#"><i>Amis</i></a> 
   <a href="#"><i>photo</i></a>
     </div>
+</div>
 </div>
 <div class="chat-sidebar">
             <div class="sidebar-name">          
@@ -43,6 +47,7 @@ export default class Profile extends Component {
             </div>
            
         </div>
+        
 </div>
     );
   }
